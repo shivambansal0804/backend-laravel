6136c4dafc7a5d9f75ea0deed8f7668d18e4acec
Manish sahani <manishsahani1351111@gmail.com>
2018-10-18 02:44:25 +0530

    Heroku installed

--- resources/views/users/superuser/index.blade.php
+++ resources/views/users/superuser/index.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 mt-md-5 text-center">
-                <div class="mt-3">
-                    <img src="{{ asset('svg/admin.svg')}}" alt="" srcset="" width="250">
+    <section>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 mt-md-5 text-center">
+                    <div class="mt-3">
+                        <img src="{{ asset('svg/admin.svg')}}" alt="" srcset="" width="250">
+                    </div>
+                    <br>
+                    <h2>Welcome {{ auth()->user()->name }}</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit eligendi, rem autem porro provident veniam aperiam dicta
+                        deserunt nemo reprehenderit illo eum soluta eos atque beatae exercitationem ad omnis deleniti.
+                    </p>
                 </div>
-                <br>
-                <h2>Welcome {{ auth()->user()->name }}</h2>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit eligendi, rem autem porro provident veniam aperiam dicta deserunt 
-                    nemo reprehenderit illo eum soluta eos atque beatae exercitationem ad omnis deleniti.
-                </p>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
