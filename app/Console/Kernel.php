2d272de6168eb363196a80f8dc2b1efd05a81fa9
Manish sahani <manishsahani1351111@gmail.com>
2018-10-21 15:35:36 +0530

    Kernel updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('dtutimes:daily-report')->dailyAt('23:55');
+        $schedule->command('dtutimes:weekly-report')->mondays();
     }
 
     /**
